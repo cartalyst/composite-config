756ca721fa0217ac6c46f9d3a54af8051cc6a89e
Ben Corlett <bencorlett@me.com>
2013-01-09 15:56:23 +1100

    Making the service provider extend the right class.
    Signed-off-by: Ben Corlett <bencorlett@me.com>

--- src/Cartalyst/CompositeConfig/CompositeConfigServiceProvider.php
+++ src/Cartalyst/CompositeConfig/CompositeConfigServiceProvider.php
@@ -20,7 +20,7 @@
 
 use Illuminate\Support\ServiceProvider;
 
-class CompositeConfigServiceProvider {
+class CompositeConfigServiceProvider extends ServiceProvider {
 
 	/**
 	 * Register the service provider.
